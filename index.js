93063bfcc2154604dbbd90627a427f41160d3ee4
Pathania1204 <garimapathania23@gmail.com>
2026-02-27 11:42:29 +0530

    Updated

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
-import cors from "cors";
 import dbConnect from "./config/db.js";
 import userRouter from "./routers/userRoute.js";
-dotenv.config();  
+import cors from "cors";
+
+dotenv.config();
+
 const app = express();
+
 app.use(express.json());
 app.use(cors());
-app.use("/api/users", userRouter);
-const startServer = async () => {
-  await dbConnect(); 
-  app.listen(8080, () => console.log("Server Started on 8080"));
-};
 
-startServer();
+
+await dbConnect();
+
+
+app.use("/api/users", userRouter);
+
+app.get("/", (req, res) => {
+  res.send("LMS Backend is running ");
+});
+
+
+export default app;
